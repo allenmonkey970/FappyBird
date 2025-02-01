61bdc1ced929a191a31dfade3fe0e46baac4e70f
antho <anthonyzolmora@outlook.com>
2025-02-01 15:35:10 -0700

    added icon class to create icon for exe file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+class icon {
+public:
+    icon(const std::string& imageFile) {
+        if (!image.loadFromFile(imageFile)) {
+            std::cerr << "Failed loading icon" << std::endl;
+        }
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.setIcon(image);
+    }
+
+private:
+    sf::Image image;
+};
+
+
+class background {
+public:
+    background(const std::string& textureFile) : sprite(texture) {
+        if (!texture.loadFromFile(textureFile)) {
+            std::cerr << "Failed loading pipe texture" << std::endl;
+        }
+        sprite.setTextureRect(sf::IntRect({0, 0}, {288, 512}));
+        sprite.setScale({1.7, 1.7});
+        texture.setSmooth(true);
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(sprite);
+    }
+
+private:
+    sf::Texture texture;
+    sf::Sprite sprite;
+};
+
+
 class FappyBird {
 public:
     FappyBird(const std::string& textureFile, float x, float y) : sprite(texture) {
@@ -29,6 +67,7 @@ public:
         }
         sprite.setTextureRect(sf::IntRect({0, 0}, {52, 320}));
         sprite.setPosition({x, y});
+        texture.setSmooth(true);
     }
 
     void draw(sf::RenderWindow& window) {
@@ -44,6 +83,10 @@ int main() {
     auto window = sf::RenderWindow(sf::VideoMode({480, 800u}), "Fappy Bird");
     window.setFramerateLimit(144);
 
+    //basic
+    icon icon("src/assets/icon.png");
+    background background("src/assets/background-night.png");
+
     FappyBird fappyBird("src/assets/FappyBird.png", 20, 30);
     Pipe pipe("src/assets/pipe-green.png", 200, 300);
 
@@ -54,7 +97,9 @@ int main() {
             }
         }
 
-        window.clear(sf::Color::White);
+        window.clear();
+        icon.draw(window);
+        background.draw(window);
         fappyBird.draw(window);
         pipe.draw(window);
         window.display();
