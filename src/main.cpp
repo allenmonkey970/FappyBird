782f84b830a6054fd012d4f87d9345686b1a5b6e
antho <anthonyzolmora@outlook.com>
2025-02-01 12:50:40 -0700

    etc

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Fappy bird");
     window.setFramerateLimit(144);
     sf::Texture texture;
-    if (!(texture.loadFromFile("src\\packman.png", false, sf::IntRect( {10, 10}, {100, 100})))){
+    if (!(texture.loadFromFile("C:\\Users\\antho\\CLionProjects\packman\\src\\FappyBird.png", false, sf::IntRect( {10, 10}, {100, 100})))){
         std::cerr << "Failed to load texture" << std::endl;
         return -1;
     }
