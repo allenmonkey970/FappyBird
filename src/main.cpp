1f2ae9a07e880d8ee8c5daf864786258b4627ea7
antho <anthonyzolmora@outlook.com>
2025-02-01 13:45:40 -0700

    assets added and added to screen with classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,62 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Fappy bird");
-    window.setFramerateLimit(144);
-    sf::Texture texture;
-    if (!(texture.loadFromFile("C:\\Users\\antho\\CLionProjects\packman\\src\\FappyBird.png", false, sf::IntRect( {10, 10}, {100, 100})))){
-        std::cerr << "Failed to load texture" << std::endl;
-        return -1;
+class FappyBird {
+public:
+    FappyBird(const std::string& textureFile) : sprite(texture) {
+        if (!texture.loadFromFile(textureFile)) {
+            std::cerr << "Failed loading Fappy texture" << std::endl;
+        }
+        sprite.setTextureRect(sf::IntRect({0, 0}, {34, 24}));
+        sprite.setScale({2.2, 2.2});
+        sprite.setPosition({50, 50});
     }
-    sf::Sprite sprite(texture);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    void draw(sf::RenderWindow& window) {
+        window.draw(sprite);
+    }
+
+private:
+    sf::Texture texture;
+    sf::Sprite sprite;
+};
+
+class Pipe {
+public:
+    Pipe(const std::string& textureFile, float x, float y) : sprite(texture) {
+        if (!texture.loadFromFile(textureFile)) {
+            std::cerr << "Failed loading pipe texture" << std::endl;
+        }
+        sprite.setTextureRect(sf::IntRect({0, 0}, {52, 320}));
+        sprite.setPosition({100, 200});
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(sprite);
+    }
+
+private:
+    sf::Texture texture;
+    sf::Sprite sprite;
+};
+
+int main() {
+    auto window = sf::RenderWindow(sf::VideoMode({480, 800u}), "Fappy Bird");
+    window.setFramerateLimit(144);
+
+    FappyBird fappyBird("src/assets/FappyBird.png");
+    Pipe pipe("src/assets/pipe-green.png", 200, 300);
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
 
-        window.clear();
-        window.draw(sprite);
+        window.clear(sf::Color::White);
+        fappyBird.draw(window);
+        pipe.draw(window);
         window.display();
     }
 }
